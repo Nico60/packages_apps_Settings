ef21aa608ee7d219bee4f195e6300594921f30f1
sethyx <sethyx@gmail.com>
2012-04-03 23:09:04 +0200

    option to disable camera sounds (1/3)
    Change-Id: Ibc61515e78ecfa1d438b3770b02284c4cb9d7f23
    Signed-off-by: sethyx <sethyx@gmail.com>
    
    Conflicts:
    
    	src/com/android/settings/SoundSettings.java

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -39,6 +39,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemProperties;
 import android.os.Vibrator;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -91,6 +92,9 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_VIBRATE_DURING_CALLS =
             Settings.System.NOTIFICATION_VIBRATE_DURING_ALERTS_DISABLED;
     private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds_enabled";
+    private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
+    private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
+
     private static final String KEY_POWER_NOTIFICATIONS = "power_notifications";
     private static final String KEY_POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
     private static final String KEY_POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
@@ -116,6 +120,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private ListPreference mVolumeOverlay;
     private ListPreference mRingMode;
     private CheckBoxPreference mSoundEffects;
+    private CheckBoxPreference mCameraSounds;
     private Preference mMusicFx;
     private Preference mRingtonePreference;
     private Preference mNotificationPreference;
@@ -211,6 +216,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         if (!Utils.hasVolumeRocker(getActivity())) {
             getPreferenceScreen().removePreference(findPreference(KEY_VOLUME_ADJUST_SOUNDS));
         }
+
+        mCameraSounds = (CheckBoxPreference) findPreference(KEY_CAMERA_SOUNDS);
+        mCameraSounds.setPersistent(false);
+        mCameraSounds.setChecked(SystemProperties.getBoolean(
+                PROP_CAMERA_SOUND, true));
 
         mRingtonePreference = findPreference(KEY_RINGTONE);
         mNotificationPreference = findPreference(KEY_NOTIFICATION_SOUND);
@@ -405,6 +415,9 @@ public class SoundSettings extends SettingsPreferenceFragment implements
             } else {
                 mAudioManager.unloadSoundEffects();
             }
+
+        } else if (preference == mCameraSounds) {
+            SystemProperties.set(PROP_CAMERA_SOUND, mCameraSounds.isChecked() ? "1" : "0");
 
         } else if (preference == mMusicFx) {
             // let the framework fire off the intent
