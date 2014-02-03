e3bc6216822f48f06b7d254a244de985ddca6951
Lokesh Chamane <lokesh.c703@gmail.com>
2014-02-03 20:25:36 +0100

    Show Navigation bar menu for every device now
    Change-Id: I8c605b6444115bf5adbadd9b741749ef50d162f8
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>
    
    Conflicts:
    
    	src/com/android/settings/cyanogenmod/SystemUiSettings.java

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -90,8 +90,6 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
                 mExpandedDesktopNoNavbarPref.setOnPreferenceChangeListener(this);
                 mExpandedDesktopNoNavbarPref.setChecked(expandedDesktopValue > 0);
                 expandedCategory.removePreference(mExpandedDesktopPref);
-                // Hide navigation bar category
-                prefScreen.removePreference(findPreference(CATEGORY_NAVBAR));
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error getting navigation bar status");
