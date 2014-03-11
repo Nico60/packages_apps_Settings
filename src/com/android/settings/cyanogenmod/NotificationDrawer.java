7761d68d5ee2bee796cbae4c20313e502bd77fd6
Lars Greiss <kufikugel@googlemail.com>
2014-03-11 21:51:57 +0100

    [2/2 Notification Shortcuts by SlimRoms
    this will add back slims notification shorcuts feature as well
    we add common slim classes we use for navbar navring pie etc
    see fb commit for more details
    
    PS2: update Author & message
    PS3: again message
    PS4: Rebase
    PS5: Rebase and change from slim's to existing dslv
    PS6: Back to separate dslv since the one we already have doesn't seem to work.
    
    NEED this commit to work -> http://review.pac-rom.com/#/c/449/
    
    Change-Id: I58087df9125ccff490b15cb8e07a95590f8e4eb7
    
    Conflicts:
    
    	res/values/pac_arrays.xml
    	res/values/pac_strings.xml
    	res/xml/notification_drawer.xml
    	src/com/android/settings/cyanogenmod/NotificationDrawer.java

--- src/com/android/settings/cyanogenmod/NotificationDrawer.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawer.java
@@ -17,13 +17,17 @@
 package com.android.settings.cyanogenmod;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.util.slim.DeviceUtils;
 
 public class NotificationDrawer extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -31,7 +35,10 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
 
     private static final String UI_COLLAPSE_BEHAVIOUR = "notification_drawer_collapse_on_dismiss";
 
+    private static final String PREF_NOTIFICATION_HIDE_CARRIER = "notification_hide_carrier";
+
     private ListPreference mCollapseOnDismiss;
+    CheckBoxPreference mHideCarrier;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,12 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
         mCollapseOnDismiss.setValue(String.valueOf(collapseBehaviour));
         mCollapseOnDismiss.setOnPreferenceChangeListener(this);
         updateCollapseBehaviourSummary(collapseBehaviour);
+
+        mHideCarrier = (CheckBoxPreference) findPreference(PREF_NOTIFICATION_HIDE_CARRIER);
+        boolean hideCarrier = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NOTIFICATION_HIDE_CARRIER, 0) == 1;
+        mHideCarrier.setChecked(hideCarrier);
+        mHideCarrier.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -56,6 +69,11 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS, value);
             updateCollapseBehaviourSummary(value);
+            return true;
+        } else if (preference == mHideCarrier) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_HIDE_CARRIER,
+                    (Boolean) objValue ? 1 : 0);
             return true;
         }
 
