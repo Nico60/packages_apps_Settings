10847a4f7dce3175eae47e19dc7d557707d0fd09
Roman Birg <roman@cyngn.com>
2014-04-29 14:45:17 -0700

    Settings: Disable slider shortcuts with custom lockscreen
    These are not available with the custom lockscreen.
    
    Change-Id: Ib67b97b597da0cf06d9d36b4fe7b40b55d0e2425
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    
    	src/com/android/settings/cyanogenmod/LockscreenInterface.java

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -70,8 +70,10 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private static final String KEY_DISABLE_FRAME = "lockscreen_disable_frame";
     private static final String KEY_ENABLE_MAXIMIZE_WIGETS = "lockscreen_maximize_widgets";
     private static final String KEY_LOCKSCREEN_MODLOCK_ENABLED = "lockscreen_modlock_enabled";
+    private static final String KEY_LOCKSCREEN_TARGETS = "lockscreen_targets";
     private static final String KEY_SEE_THROUGH = "see_through";
     private static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
+
     private static final String LOCKSCREEN_WALLPAPER_TEMP_NAME = ".lockwallpaper";
 
     private static final int REQUEST_PICK_WALLPAPER = 201;
@@ -85,6 +87,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private CheckBoxPreference mSeeThrough;
     private ListPreference mLockBackground;
     private ListPreference mBatteryStatus;
+    private Preference mLockscreenTargets;
 
     private ChooseLockSettingsHelper mChooseLockSettingsHelper;
     private LockPatternUtils mLockUtils;
@@ -111,6 +114,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
         mEnableCameraWidget = (CheckBoxPreference) findPreference(KEY_ENABLE_CAMERA);
         mEnableMaximizeWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_MAXIMIZE_WIGETS);
+        mLockscreenTargets = findPreference(KEY_LOCKSCREEN_TARGETS);
 
         // lockscreen see through
         mSeeThrough = (CheckBoxPreference) findPreference(KEY_SEE_THROUGH);
@@ -245,6 +249,9 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         }
         if (mEnableMaximizeWidgets != null) {
             mEnableMaximizeWidgets.setEnabled(enabled);
+        }
+        if (mLockscreenTargets != null) {
+            mLockscreenTargets.setEnabled(enabled);
         }
     }
 
