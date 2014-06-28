519a7d25001c48030724532cbc23d839290fadb0
Nico60 <ndhouailly@gmail.com>
2014-06-28 18:19:07 +0200

    Revert "Add Option to HALO On / Off"
    This reverts commit d52bf5b36748453537d2c56946880ed9b2ee500e.
    
    Conflicts:
    	res/values/spirit_strings.xml
    	res/xml/halo_settings.xml
    	src/com/android/settings/paranoid/Halo.java
    
    Change-Id: I1d7388454bbb144cddb9ce3a5b1a0c52dd571915

--- src/com/android/settings/paranoid/Halo.java
+++ src/com/android/settings/paranoid/Halo.java
@@ -52,7 +52,6 @@ public class Halo extends SettingsPreferenceFragment
     private static final String PREF_HALO_BUBBLE_TEXT_COLOR = "halo_bubble_text_color";
     private static final String PREF_HALO_NUMBER_TEXT_COLOR = "halo_number_text_color";
     private static final String PREF_HALO_NUMBER_CONTAINER_COLOR = "halo_number_container_color";
-    private static final String KEY_HALO_ENABLED = "halo_enabled";
 
     private ListPreference mHaloState;
     private ListPreference mHaloSize;
@@ -68,7 +67,6 @@ public class Halo extends SettingsPreferenceFragment
     private ColorPickerPreference mHaloBubbleTextColor;
     private ColorPickerPreference mHaloNumberTextColor;
     private ColorPickerPreference mHaloNumberContainerColor;
-    private CheckBoxPreference mHaloEnabled;
 
     private Context mContext;
     private INotificationManager mNotificationManager;
@@ -83,10 +81,6 @@ public class Halo extends SettingsPreferenceFragment
 
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
-
-        mHaloEnabled = (CheckBoxPreference) findPreference(KEY_HALO_ENABLED);
-        mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_ENABLED, 0) == 1);
 
         mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
@@ -163,11 +157,7 @@ public class Halo extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if  (preference == mHaloEnabled) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
-                    ? 1 : 0);
-        } else if  (preference == mHaloHide) {
+        if (preference == mHaloHide) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_HIDE, mHaloHide.isChecked()
                     ? 1 : 0);
