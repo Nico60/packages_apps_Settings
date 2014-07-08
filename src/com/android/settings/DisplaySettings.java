3da35d90d0cc734da70cda1d016c98e2525c7a23
Roman Birg <roman@cyngn.com>
2014-07-08 15:06:29 -0700

    Settings: update default expanded desktop style
    Change-Id: I6deb3bdf9a4099c67a63c4afb7909eff24ec574b
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -161,7 +161,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 (CheckBoxPreference) findPreference(KEY_EXPANDED_DESKTOP_NO_NAVBAR);
 
         int expandedDesktopValue = Settings.System.getInt(getContentResolver(),
-                Settings.System.EXPANDED_DESKTOP_STYLE, 0);
+                Settings.System.EXPANDED_DESKTOP_STYLE, 2);
 
         try {
             boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
