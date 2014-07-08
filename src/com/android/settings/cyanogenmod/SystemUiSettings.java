a1c9fb3f3b9a938f7f7953cb8a07a9a214899fe0
liquid0624 <rjbirnesser@gmail.com>
2014-07-08 19:46:23 -0400

    Settings: create app circle sidebar settings
    Move app circle sidebar to its own preference under navigation
    This also adds options to adjust the trigger areas for sidebar
    
    Conflicts:
    	res/values/liquid_strings.xml
    	res/xml/liquid_navigation_settings.xml
    	src/com/android/settings/liquid/NavigationSettings.java
    
    Change-Id: I338bd3b6a15ff9a8295aa0ed909d9492210c1c18

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -16,60 +16,22 @@
 
 package com.android.settings.cyanogenmod;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.ActivityManagerNative;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.ComponentName;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.ResolveInfo;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.os.SystemProperties;
-import android.os.UserHandle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Slog;
-import android.view.IWindowManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.WindowManagerGlobal;
-import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.settings.chameleonos.AppMultiSelectListPreference;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.lang.Thread;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class SystemUiSettings extends SettingsPreferenceFragment  implements
-        Preference.OnPreferenceChangeListener {
+public class SystemUiSettings extends SettingsPreferenceFragment {
     private static final String TAG = "SystemSettings";
 
     private static final String KEY_SCREEN_GESTURE_SETTINGS = "touch_screen_gesture_settings";
-    private static final String PREF_INCLUDE_APP_CIRCLE_BAR_KEY = "app_circle_bar_included_apps";
-
-    private AppMultiSelectListPreference mIncludedAppCircleBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,44 +40,9 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         addPreferencesFromResource(R.xml.system_ui_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
-        mIncludedAppCircleBar = (AppMultiSelectListPreference) prefScreen.findPreference(PREF_INCLUDE_APP_CIRCLE_BAR_KEY);
-        Set<String> includedApps = getIncludedApps();
-        if (includedApps != null) mIncludedAppCircleBar.setValues(includedApps);
-        mIncludedAppCircleBar.setOnPreferenceChangeListener(this);
-
         Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
                 getPreferenceScreen(), KEY_SCREEN_GESTURE_SETTINGS);
 
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mIncludedAppCircleBar) {
-            storeIncludedApps((Set<String>) objValue);
-            return true;
-        }
-
-        return false;
-    }
-
-    private Set<String> getIncludedApps() {
-        String included = Settings.System.getString(getActivity().getContentResolver(),
-                Settings.System.WHITELIST_APP_CIRCLE_BAR);
-        if (TextUtils.isEmpty(included)) {
-            return null;
-        }
-        return new HashSet<String>(Arrays.asList(included.split("\\|")));
-    }
-
-    private void storeIncludedApps(Set<String> values) {
-        StringBuilder builder = new StringBuilder();
-        String delimiter = "";
-        for (String value : values) {
-            builder.append(delimiter);
-            builder.append(value);
-            delimiter = "|";		 
-		}
-        Settings.System.putString(getActivity().getContentResolver(),
-                Settings.System.WHITELIST_APP_CIRCLE_BAR, builder.toString());
-    }
 }
