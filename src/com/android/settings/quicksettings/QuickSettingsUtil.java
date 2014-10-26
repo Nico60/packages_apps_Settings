DirtyUnicorns/android_packages_apps_Settings

Dynamic Color Changer Tile [2/2]
Change-Id: I298d355e98acf5fb2763353397999df5e8b0cf18

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -68,6 +68,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_HEADSUP;
 import static com.android.internal.util.slim.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.slim.QSConstants.TILE_CPUFREQ;
 import static com.android.internal.util.slim.QSConstants.TILE_DIRTYTWEAKS;
+import static com.android.internal.util.slim.QSConstants.TILE_DYNAMICCOLORS;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -210,6 +211,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NAVBAR, R.string.title_tile_navbar,
                 "com.android.systemui:drawable/ic_qs_navbar_on"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_DYNAMICCOLORS, R.string.title_tile_dynamiccolors,
+                "com.android.systemui:drawable/ic_qs_dynamiccolors_on"));
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_HEADSUP, R.string.title_tile_headsup,
                 "com.android.systemui:drawable/ic_qs_headsup_on"));

Nico60/packages_apps_Settings

Conflicts:
	res/values/custom_strings.xml
	src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java

src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -59,6 +59,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_PIE;
 import static com.android.internal.util.cm.QSConstants.TILE_GESTUREPANEL;
 import static com.android.internal.util.cm.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.cm.QSConstants.TILE_SPIRIT;
+import static com.android.internal.util.cm.QSConstants.TILE_DYNAMICCOLORS;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -190,6 +191,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NAVBAR, R.string.title_navbar_tile,
                 "com.android.systemui:drawable/ic_qs_navbar_on"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_DYNAMICCOLORS, R.string.title_tile_dynamiccolors,
+                "com.android.systemui:drawable/ic_qs_dynamiccolors_on"));
 	registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_POWER, R.string.title_tile_power,
                 "com.android.systemui:drawable/ic_qs_powermenu"));