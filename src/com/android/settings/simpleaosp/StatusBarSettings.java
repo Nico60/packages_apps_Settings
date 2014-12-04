a92b4e5d76ab57b55da3c5edea545d9e75dd3f62
Lars Greiss <kufikugel@googlemail.com>
2014-12-04 14:43:00 +0500

    Settings: add quick pulldown listpreference
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/settings/simpleaosp/StatusBarSettings.java
    
    Conflicts:
    	src/com/android/settings/simpleaosp/StatusBarSettings.java
    
    Conflicts:
    	res/values/arrays.xml
    	res/values/strings.xml
    	src/com/android/settings/simpleaosp/StatusBarSettings.java
    
    Change-Id: I8b994d8f59c17617583b43b53e04cbd67b34873c
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -1,11 +1,20 @@
 package com.android.settings.simpleaosp;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.preference.Preference;
 import com.android.settings.R;
 import android.provider.Settings;
 import com.android.settings.SettingsPreferenceFragment;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class StatusBarSettings extends SettingsPreferenceFragment {
 
@@ -14,6 +23,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
     private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
 
     private PreferenceScreen mClockStyle;
+    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
+
+    private PreferenceScreen mClockStyle;
+    private ListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -23,11 +36,36 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
 
 	mClockStyle = (PreferenceScreen) prefSet.findPreference(KEY_STATUS_BAR_CLOCK);
         updateClockStyleDescription();
+       
+	mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+        if (!Utils.isPhone(getActivity())) {
+            prefs.removePreference(mQuickPulldown);
+        } else {
+            // Quick Pulldown
+            mQuickPulldown.setOnPreferenceChangeListener(this);
+            int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
+            mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
+            updateQuickPulldownSummary(statusQuickPulldown);
+        }
     }
     @Override
     public void onResume() {
         super.onResume();
         updateClockStyleDescription();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQuickPulldown) {
+            int statusQuickPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    statusQuickPulldown);
+            updateQuickPulldownSummary(statusQuickPulldown);
+            return true;
+        }
+        return false;
     }
 
     private void updateClockStyleDescription() {
@@ -40,5 +78,21 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
         } else {
             mClockStyle.setSummary(getString(R.string.disabled_string));
          }
+    }
+
+    private void updateQuickPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else {
+            Locale l = Locale.getDefault();
+            boolean isRtl = TextUtils.getLayoutDirectionFromLocale(l) == View.LAYOUT_DIRECTION_RTL;
+            String direction = res.getString(value == 2
+                    ? (isRtl ? R.string.quick_pulldown_right : R.string.quick_pulldown_left)
+                    : (isRtl ? R.string.quick_pulldown_left : R.string.quick_pulldown_right));
+            mQuickPulldown.setSummary(res.getString(R.string.summary_quick_pulldown, direction));
+        }
     }
 }
