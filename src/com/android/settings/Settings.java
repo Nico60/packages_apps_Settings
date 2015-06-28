DirtyUnicorns/android_packages_apps_Settings

NavBar tile [2/3]
Change-Id: I684e370b813d03782d01e2abc0d28f06560a4cf0

src/com/android/settings/Settings.java
@@ -103,5 +103,6 @@ public class Settings extends SettingsActivity {
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DuSettingsActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class NavBarActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -114,6 +114,7 @@ import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
+import com.dirtyunicorns.dutweaks.fragments.NavBar;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -302,7 +303,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             DirtyTweaks.class.getName(),
-            HeadsUpSettings.class.getName()
+            HeadsUpSettings.class.getName(),
+            NavBar.class.getName()
     };
 
 

Nico60/packages_apps_Settings

Conflicts:
	AndroidManifest.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java
Change-Id: Id1151ccc0e2a47c94c2cce41062181d64bc30b9f

src/com/android/settings/Settings.java
@@ -107,4 +107,5 @@ public class Settings extends SettingsActivity {
     public static class AppCircleBarActivity extends SettingsActivity { /* empty */ }
     public static class AmbientDisplaySettingsActivity extends SettingsActivity { /* empty */ }
     public static class ButtonBrightnessSettings extends SettingsActivity { /* empty */ }
+    public static class NavbarSettingsActivity extends SettingsActivity { /* empty */ }
 }

src/com/android/settings/SettingsActivity.java
@@ -80,6 +80,7 @@ import com.android.settings.dashboard.NoHomeDialogFragment;
 import com.android.settings.dashboard.SearchResultsSummary;
 import com.android.settings.deviceinfo.Memory;
 import com.android.settings.deviceinfo.UsbSettings;
+import com.android.settings.du.AmbientSettings;
 import com.android.settings.fuelgauge.BatterySaverSettings;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.notification.NotificationAppList;
@@ -113,10 +114,10 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
-import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
 import com.dirtyunicorns.dutweaks.fragments.AppCircleBar;
-import com.android.settings.du.AmbientSettings;
 import com.dirtyunicorns.dutweaks.fragments.ButtonBrightnessSettings;
+import com.dirtyunicorns.dutweaks.fragments.HeadsUpSettings;
+import com.dirtyunicorns.dutweaks.fragments.NavbarSettings;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -313,7 +314,8 @@ public class SettingsActivity extends Activity
             HeadsUpSettings.class.getName(),
             AppCircleBar.class.getName(),
             AmbientSettings.class.getName(),
-            ButtonBrightnessSettings.class.getName()
+            ButtonBrightnessSettings.class.getName(),
+            NavbarSettings.class.getName()
     };
 
 